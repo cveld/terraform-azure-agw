eba87f2b5624e45a65cc82819f87c68b97ec5d12
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2022-09-15 16:32:41 +0200

    feat: add terratest validation (#3)

---
+++ tests/application_gateway_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestApplyNoError(t *testing.T) {
+	t.Parallel()
+
+	tests := []string{
+		"../examples/simple",
+	}
+
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			terraformOptions := &terraform.Options{
+				TerraformDir: test,
+				NoColor:      true,
+				// Vars: map[string]interface{}{
+				// 	"digicert_password": os.Getenv("DIGICERT_PASSWORD"),
+				// },
+			}
+
+			terraform.WithDefaultRetryableErrors(t, &terraform.Options{})
+
+			defer terraform.Destroy(t, terraformOptions)
+			terraform.InitAndApply(t, terraformOptions)
+		})
+	}
+}
